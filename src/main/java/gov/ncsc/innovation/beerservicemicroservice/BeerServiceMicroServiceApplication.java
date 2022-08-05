fdc59c126432245b3fd3d84f73c923227b5831c6
thomas.degan <thomas.degan@caci.com>
2022-08-05 15:09:08 +0000

    Initial Commit

---
+++ src/main/java/gov/ncsc/innovation/beerservicemicroservice/BeerServiceMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package gov.ncsc.innovation.beerservicemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerServiceMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/ncsc/innovation/beerservicemicroservice/BeerServiceMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.ncsc.innovation.beerservicemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
